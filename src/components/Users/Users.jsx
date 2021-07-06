3118f80e4af995c9121c076f90c69ec3073b3ea9
Pavel Mulyar <p.mulyar@astoundcommerce.com>
2021-07-06 15:13:41 +0300

    - refactored users to class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,48 +3,43 @@ import React from "react";
 import axios from "axios";
 import userPhoto from '../../assets/images/man.png';
 
-const Users = (props) => {
-    const getUsers = () => {
-        if (props.users.length === 0) {
-
-            axios.get('https://social-network.samuraijs.com/api/1.0/users?count=40').then(responce => {
-
-                props.setUsers(responce.data.items);
-            });
-
-        }
+class Users extends React.Component {
+    componentDidMount(props) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users?count=40').then(responce => {
+            this.props.setUsers(responce.data.items);
+        });
     }
 
+    render() {
+        return <div>
+            <h1>Users</h1>
 
-    return <div>
-        <h1>Users</h1>
-        <button onClick={getUsers}>Get Users</button>
+            {
+                this.props.users.map(u =>
 
-        {
-            props.users.map(u =>
-
-                <div key={u.id} className={s.user}>
-                    <div>
-                        <img className={s.ava} src={u.photos.small ? u.photos.small : userPhoto} alt=""/> <br/>
-                        {
-                            <button onClick={() => {
-                                props.toggleFollowUserAC(u.id)
-                            }}>{u.follow ? 'Unfollow' : 'Follow'}</button>
-                        }
-                    </div>
-                    <div className={s.info}>
+                    <div key={u.id} className={s.user}>
                         <div>
-                            <span>{u.name}</span>
-                            <p>{u.status}</p>
+                            <img className={s.ava} src={u.photos.small ? u.photos.small : userPhoto} alt=""/> <br/>
+                            {
+                                <button onClick={() => {
+                                    this.props.toggleFollowUserAC(u.id)
+                                }}>{u.follow ? 'Unfollow' : 'Follow'}</button>
+                            }
                         </div>
-                        <div className={s.location}>
-                            <p>u.location.country</p>
-                            <p>u.location.city</p>
+                        <div className={s.info}>
+                            <div>
+                                <span>{u.name}</span>
+                                <p>{u.status}</p>
+                            </div>
+                            <div className={s.location}>
+                                <p>u.location.country</p>
+                                <p>u.location.city</p>
+                            </div>
                         </div>
-                    </div>
-                </div>)
-        }
-    </div>
+                    </div>)
+            }
+        </div>
+    }
 }
 
 export default Users;
